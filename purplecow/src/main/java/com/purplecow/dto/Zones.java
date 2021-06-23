cbf12028bd2f63c437806122a8dabdd6c1615041
gangsan89 <ksk6736@naver.com>
2021-06-23 20:43:58 +0900

    [kimk] 내 위치를 가져오는건 성공인데 이걸 지도 첫 화면에서 가져오진 못한다. 값을 못가져 오는거 같다, 이 부분 해결하면서 존 정보 관련된 backend쪽 만들어야함

---
+++ purplecow/src/main/java/com/purplecow/dto/Zones.java
@@ -0,0 +1,14 @@
+package com.purplecow.dto;
+
+import lombok.Data;
+
+@Data
+public class Zones {
+	
+	private int id;
+	private String name;
+	private double latitude;
+	private double longitude;
+	private String address;
+	
+}
